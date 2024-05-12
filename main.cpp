863aa264fc9874ff8344c3e90345db858658fe60
Leonid <114904902+Spermoslav@users.noreply.github.com>
2024-05-12 11:56:33 +0300

    create project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "menu.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.setMinimumSize(250, 500);
+    w.setGeometry(200, 200, 400, 650);
+    w.show();
+    return a.exec();
+}
