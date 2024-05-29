4fd9e6dde9fa9871bbb2b5a8de86c2a04e77e062
Leonid <114904902+Spermoslav@users.noreply.github.com>
2024-05-29 21:01:18 +0300

    add define

--- defines.h
+++ defines.h
@@ -1,10 +1,11 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-//#define DEBUG
+#define DEBUG
 
 #ifdef DEBUG
-    #define SSMAKER_WINDOW // открывает меню скриншота в окне
+//    #define SSMAKER_WINDOW // открывает меню скриншота в окне
+    #define SSMAKER_START  // сразу запускает ScreenShotMaker
 
 #endif // DEBUG
 #endif // DEFINES_H
