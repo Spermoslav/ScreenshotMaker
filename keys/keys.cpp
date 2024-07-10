ff8a478d4862c66d56d2aca630da82b2dafa23b8
Leonid <114904902+Spermoslav@users.noreply.github.com>
2024-07-10 18:32:48 +0300

    upd KeyShortcut
    Сделал его шаблонным, соответственно перенес реализацию в .h файл
    также убрал operator = и += за ненадобностью

--- keys/keys.cpp
+++ keys/keys.cpp
@@ -1,95 +1,9 @@
 #include "keys.h"
 #include <QDebug>
 
-KeyShortcut::KeyShortcut(const std::initializer_list<Qt::Key> &key)
-{
-    for(Qt::Key k : key) {
-        if(std::find(keys.begin(), keys.end(), KeyStatus(k, false)) == keys.end())
-            keys.push_back(KeyStatus(k, false));
-    }
-}
-
-KeyShortcut::KeyShortcut(Qt::Key key)
-{
-    keys.push_back(KeyStatus(key, false));
-}
 
 
-KeyShortcut &KeyShortcut::operator =(const KeyShortcut &ks)
-{
-    keys = ks.keys;
-    return *this;
-}
 
-KeyShortcut &KeyShortcut::operator +=(const KeyShortcut &ks)
-{
-    for(KeyStatus k : ks.keys) {
-        if(std::find(keys.begin(), keys.end(), k) == keys.end()) {
-            keys.push_back(k);
-        }
-    }
-    return *this;
-}
-
-void KeyShortcut::addKey(Qt::Key key)
-{
-    if(std::find(keys.begin(), keys.end(), KeyStatus(key, false)) == keys.end())
-        keys.push_back(KeyStatus(key, false));
-}
-
-void KeyShortcut::releaseKeys()
-{
-    for(KeyStatus &p : keys) {
-        p.second = false;
-    }
-}
-
-void KeyShortcut::keyPress(Qt::Key key)
-{
-    if(func){
-        bool allPress = false;
-        (*std::find(keys.begin(), keys.end(), KeyStatus(key, false))).second = true;  // кнопке, которую нажали присваеваем true
-
-        for(const KeyStatus &m : keys) {
-            if(m.second == true) allPress = true;                   // проверяем, все ли кнопки нажаты
-            else {
-                allPress = false;
-                break;
-            }
-        }
-
-        if(allPress) {                      // если все кнопки нажаты,
-            releaseKeys();                  // присваиваем всем кнопкам false
-            (funcObj->*func)();             // и вызываем функцию.
-        }
-    }
-    else {
-        qDebug() << "KeyShortcut::func == nullptr";
-    }
-}
-
-void KeyShortcut::keyRelease(Qt::Key key)
-{
-    if(func) {
-        for(KeyStatus &k : keys) {
-            if(k.first == key) {
-                k.second = false;      // кнопке, которую отжали присваеваем false
-            }
-        }
-    }
-    else {
-        qDebug() << "KeyShortcut::func == nullptr";
-    }
-}
-
-std::list<Qt::Key> KeyShortcut::shortcut() const
-{
-    std::list<Qt::Key> key;
-    for(const KeyStatus &k : keys) {
-        key.push_back(k.first);
-    }
-    return key;
-}
 
 Qt::Key winKeyToKey(int key)
 {
--- keys/keys.h
+++ keys/keys.h
@@ -1,48 +1,97 @@
 #ifndef KEYS_H
 #define KEYS_H
 
-#include "menu.h"
 #include <QString>
 #include <map>
 #include <list>
+#include "defines.h"
 
 /* KeyShortcut хранит последовательность клавиш
  * Eсли все клавиши из этой последовательности нажаты - вызывает заданную функцию
  * Необходимо через Qtшный класс, используя keyPressEvent и keyReleaseEvent,
  * вызывать keyPress и keyRelease соответственно
- * Может работать только с MainWidget (мне впадлу писать шаблон, я его написал только для этого класса)
 */
+
+template<typename T>
 class KeyShortcut
 {
 public:
     KeyShortcut() = default;
-    explicit KeyShortcut(Qt::Key key);
-    explicit KeyShortcut(const std::initializer_list<Qt::Key> &key);
+    explicit KeyShortcut(Qt::Key key)
+    {
+        keys.push_back(KeyStatus(key, false));
+    }
+    explicit KeyShortcut(const std::initializer_list<Qt::Key> &key)
+    {
+        for(Qt::Key k : key) {
+            if(std::find(keys.begin(), keys.end(), KeyStatus(k, false)) == keys.end())
+                keys.push_back(KeyStatus(k, false));
+        }
+    }
+    void setFunc(T *obj, void (T::*foo) ()) { func = foo; funcObj = obj; }
+    void addKey(Qt::Key key)                                // добавляет один key
+    {
+        if(std::find(keys.begin(), keys.end(), KeyStatus(key, false)) == keys.end())
+            keys.push_back(KeyStatus(key, false));
+    }
 
-    KeyShortcut &operator = (const KeyShortcut &ks);
-    KeyShortcut &operator += (const KeyShortcut &ks);
-
-    void setFunc(MainWidget *obj, void (MainWidget::*foo) ()) { func = foo; funcObj = obj; }
-
-    void addKey(Qt::Key key);                             // добавляет один key
     inline void clear() { keys.clear(); }                        // удаляет все ключи
 
-    void keyPress(Qt::Key key);                           // должен срабатывать, когда нажимается какая-либо клавиша
-    void keyRelease(Qt::Key key);                         // должен срабатывать, когда отжимается какая-либо клавиша
+    void keyPress(Qt::Key key)                           // должен срабатывать, когда нажимается какая-либо клавиша
+    {
+        if(func){
+            bool allPress = false;
+            (*std::find(keys.begin(), keys.end(), KeyStatus(key, false))).second = true;  // кнопке, которую нажали присваеваем true
 
+            for(const KeyStatus &m : keys) {
+                if(m.second == true) allPress = true;                   // проверяем, все ли кнопки нажаты
+                else {
+                    allPress = false;
+                    break;
+                }
+            }
+
+            if(allPress) {                      // если все кнопки нажаты,
+                releaseKeys();                  // присваиваем всем кнопкам false
+                (funcObj->*func)();             // и вызываем функцию.
+            }
+        }
+    }
+    void keyRelease(Qt::Key key)                         // должен срабатывать, когда отжимается какая-либо клавиша
+    {
+        if(func) {
+            for(KeyStatus &k : keys) {
+                if(k.first == key) {
+                    k.second = false;      // кнопке, которую отжали присваеваем false
+                }
+            }
+        }
+    }
     inline bool empty() const { return keys.empty(); }
     inline int  size()  const { return keys.size(); }
 
-    std::list<Qt::Key> shortcut() const;                  // вернет список клавиш
+    std::list<Qt::Key> shortcut() const                  // вернет список клавиш
+    {
+        std::list<Qt::Key> key;
+        for(const KeyStatus &k : keys) {
+            key.push_back(k.first);
+        }
+        return key;
+    }
 
     inline const std::list<KeyStatus> getKeys() const { return keys; }
 
 private:
-    void releaseKeys();  // всем кнопкам присваивает false
+    void releaseKeys()  // всем кнопкам присваивает false
+    {
+        for(KeyStatus &p : keys) {
+            p.second = false;
+        }
+    }
 
     std::list<KeyStatus> keys;
-    void (MainWidget::*func)()  = nullptr;
-    MainWidget *funcObj = nullptr;
+    void (T::*func)()  = nullptr;
+    T *funcObj = nullptr;
 };
 
 // Unicode Basic Latin block
