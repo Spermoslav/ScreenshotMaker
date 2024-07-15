2cf97b6fe3b3d8c96a6201f654c07199e2438850
Leonid <114904902+Spermoslav@users.noreply.github.com>
2024-07-15 19:54:40 +0300

    add active process check
    если другой процесс уже создал разделяемую память то закрываем

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 #include "keyboardhook.h"
 
 #include <QApplication>
+#include <QSharedMemory>
 
 extern HHOOK hook;
 
 int main(int argc, char *argv[])
 {
+    QSharedMemory global("key");
+    if (!global.create(1)) // если другой процесс уже создал разделяемую память то выходим
+        return 1;
+
     QApplication a(argc, argv);
     MainWidget w;
     w.setMinimumSize(250, 500);
