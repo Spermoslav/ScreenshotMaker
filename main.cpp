d1fd2a3c54235bffb38a0795c72a97d2f4788ed8
Leonid <114904902+Spermoslav@users.noreply.github.com>
2024-07-15 16:42:24 +0300

    add mainwindow flags

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main(int argc, char *argv[])
     MainWidget w;
     w.setMinimumSize(250, 500);
     w.setGeometry(200, 200, 400, 650);
+    w.setWindowFlag(Qt::Tool);
     w.show();
+    QApplication::setQuitOnLastWindowClosed(false);
 
     hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallBack, NULL, 0);
 
